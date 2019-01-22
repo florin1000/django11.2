dfafe5d4a767be984f6c1ffa95b1a7720d9cfc10
florin <quentinbargan@gmail.com>
2019-01-22 13:32:26 +0200

    from function views to class views

--- django11_2/src/application/urls.py
+++ django11_2/src/application/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
-from restaurants.views import home, about, contact
+from restaurants.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'home', home),
-    url(r'contact', contact),
-    url(r'about', about),
+    url(r'home', HomeView.as_view()),
+    url(r'contact', TemplateView.as_view(template_name='contact.html')),
+    url(r'about', TemplateView.as_view(template_name='about.html')),
 ]
--- django11_2/src/restaurants/views.py
+++ django11_2/src/restaurants/views.py
@@ -1,19 +1,20 @@
 import random
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 
 
 # Create your views here.
-def home(request):
-    num = random.randint(0, 1000)
-    names = ["Bob", "Jennifer"]
-    context = {"name": "John", "num": num, "names": names}
-    return render(request, "home.html", context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
+    # maybe a new way to update the context
+    # https://reinout.vanrees.org/weblog/2014/05/19/context.html
 
-def contact(request):
-    return render(request, "contact.html", {})
-
-
-def about(request):
-    return render(request, "about.html", {})
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        surnames = ["JOhn", "Emil"]
+        context.update({"surnames": surnames})
+        print(context)
+        return context
